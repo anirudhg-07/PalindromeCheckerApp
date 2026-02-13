0052c63e7ddf2a739ddc50ba9af68140381af8e1
anirudhg-07 <[200~anirudhg1605@gmail.com>
2026-02-13 11:14:20 +0530

    [200~UC1: Application Entry & Welcome Message~

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,11 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] argsg) {
-        System.out.println("Enter the number to check the palindrome:");
-        int a,j,i;
-        int rev = 0;
-        Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        while (a != 0) {
-            int digit = a%10;
-            rev = (rev*10) + digit;
-            a = a/10;
-        }
-        if(a == rev){
-            System.out.println("This is palindrome");
-        }
-        else{
-            System.out.println("This is not palindrome");
-        }
-        System.out.println(a);
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome checker Management");
+        System.out.println("Version:1.0");
+        System.out.println("systerm intialized successfully");
+
     }
 
 }
