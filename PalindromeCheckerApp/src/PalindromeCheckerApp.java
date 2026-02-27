825bd6bbb49f0d9bef885e1976257849b30eddc4
Anirudh G <anirudhg1605@gmail.com>
2026-02-27 09:28:00 +0530

    Add files via upload

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,13 +1,22 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "racecar";
-        String reversed = "";
+        String input = "madam";
+        char[] chars = input.toCharArray();
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
